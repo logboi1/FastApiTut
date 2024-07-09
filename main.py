2d1b76c9fc7cdfbd2e207928800891b6578fd7ee
logboi1 <ogundeleibraheem@gmail.com>
2024-07-09 09:16:18 +0100

    add task CRUD, no database

--- main.py
+++ main.py
@@ -1,100 +1,58 @@
-from enum import Enum
-from typing import Annotated
-from fastapi import FastAPI, Path, Query
+from typing import List, Optional
+from uuid import UUID, uuid4
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
 
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(
-    item_id: Annotated[
-        int, Path(description="The ID of the item to get", gt=0, le=1000)
-    ],
-):
-    return {"item_id": item_id}
+class Task(BaseModel):
+    id: Optional[UUID] = None
+    title: str
+    description: Optional[str] = None
+    completed: bool = False
 
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+tasks = []
 
 
-@app.get("/movies/{movie_id}")
-async def get_movie(movie_id: str, name: str | None = None):
-    if name:
-        return {"movie_id": movie_id, "name": name}
-    return {"movie_id": movie_id}
+@app.post("/create_task/", response_model=Task)
+async def create_task(task: Task):
+    task.id = uuid4()
+    tasks.append(task)
+    return task
 
 
-@app.get("/test_annotated/")
-async def test_annotated(
-    q: Annotated[
-        str | None,
-        Query(
-            description="Query string for the items to search in the database that have a good match",
-            min_length=3,
-            alias="item-query",
-            deprecated=True,
-        ),
-    ] = None,
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_name": "Test Annotated"}]}
-    if q:
-        results.update({"q": q})
-    return results
+@app.get("/tasks/", response_model=List[Task])
+async def get_tasks():
+    return tasks
 
 
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, description: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if description:
-        short = True  # Ensure short is False if description is present
-        item.update({"q": description})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
+@app.get("/tasks/{task_id}", response_model=Task)
+async def read_task(task_id: UUID):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.model_dump()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+@app.put("/tasks/{task_id}", response_model=Task)
+async def update_task(task_id: UUID, task_update: Task):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            updated_task = task.copy(update=task_update.dict(exclude_unset=True))
+            tasks[idx] = updated_task
+            return updated_task
+
+    raise HTTPException(status_code=404, detail="Task not found")
 
 
-@app.put("/items/{item_id}")
-async def update_item(item: Item, item_id: int):
-    return {"item_id": item_id, **item.model_dump()}
+@app.delete("/tasks/{task_id}", response_model=Task)
+async def delete_task(task_id: UUID):
+    for idx, task in enumerate(tasks):
+        if task.id == task_id:
+            return tasks.pop(idx)
 
-
-@app.get("/multiple_items/")
-async def read_items(q: Annotated[list | None, Query()] = []):
-    query_items = {"q": q}
-    return query_items
+    raise HTTPException(status_code=404, detail="Task not found")
